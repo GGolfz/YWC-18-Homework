0310a86793c5ac2c081b56d57a2a4bcebd34e335
GGolfz <wisarutgolf11@gmail.com>
2020-11-18 21:05:24 +0700

    [FIX] Duplicate data

--- components/utils/radioSelect.tsx
+++ components/utils/radioSelect.tsx
@@ -6,15 +6,19 @@ const RadioSelect = ({ current, data, handleChange, property }) => {
       <Radio.Group value={current} onChange={handleChange}>
         <Radio value="ทั้งหมด">ทั้งหมด</Radio>
         {data?.map((el, index) => {
-          return (
-            <Radio
-              style={{ display: 'block', lineHeight: '32px' }}
-              key={index}
-              value={property ? el[property] : el}
-            >
-              {property ? el[property] : el}
-            </Radio>
-          )
+          if (index > 0 && data[index] != data[index - 1]) {
+            return (
+              <Radio
+                style={{ display: 'block', lineHeight: '32px' }}
+                key={index}
+                value={property ? el[property] : el}
+              >
+                {property ? el[property] : el}
+              </Radio>
+            )
+          } else {
+            return null
+          }
         })}
       </Radio.Group>
     </Fragment>
